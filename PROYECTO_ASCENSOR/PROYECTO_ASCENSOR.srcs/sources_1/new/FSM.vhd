e2c462c12e7614b0c6b8283680bb2f7b8dc99e0b
JunjieDai2003 <162756402+JunjieDai2003@users.noreply.github.com>
2025-12-08 20:01:45 +0100

    Nuevo progreso
    He diseñado dos máquinas de estados de dos maneras, para facilitar la comprobacion, he añadido el proyecto de la practica 3 para posteriormente comprobarlo con la placa, en un principio la sintesis no dio error

--- PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/FSM.vhd
+++ PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/FSM.vhd
@@ -6,15 +6,18 @@ entity FSM is
     port(
     clk: in std_logic ;
     rst: in std_logic;
-    --00 01 10 11
-    -- no tenfgo aun muy claro si piso es in o out
-    --mi piso debe ser out para luego comunicar con display
-    --piso final debe ser in, es informacion recipida de flanco
-    -- piso final no estoy muy seguro, voy a trabajar con out
     piso_actual : out std_logic_vector(1 downto 0); -- si tengo q operar seria unsigned
     piso_final: out std_logic_vector(1 downto 0);
+    
+    
+    --salidas de maquina
     motor: out std_logic_vector(1 downto 0);
-    puerta: out std_logic
+    puerta: out std_logic;
+    
+    
+    -- inputs de usuario
+    planta_1, planta_2, planta_3, planta_4: in std_logic;
+    sensor_motor: in std_logic
     );
 end FSM;
 
@@ -34,10 +37,81 @@ architecture ascensor of fsm is
     begin
         if  rst = '0' then
                 current_state <= REPOSO;
+                s_piso_actual <="00";
+                s_piso_final <="00";
+                
          elsif rising_edge(CLK) then
                 current_state <= next_state;
+                
+                case current_state is
+                
+                  when REPOSO =>
+                    if planta_1 = '1' 
+                            then s_piso_final <= "00";
+                    elsif planta_2 = '1' 
+                            then s_piso_final <= "01";
+                    elsif planta_3 = '1' 
+                            then s_piso_final <= "10";
+                    elsif planta_4 = '1' 
+                            then s_piso_final <= "11";
+                    end if;
+                
+                    when SUBIENDO =>
+                    if sensor_motor = '1' then
+                    s_piso_actual <= s_piso_actual + 1;
+                    next_state <= ANALIZANDO;
+                    end if;
+                    
+                    when BAJANDO => 
+                    if sensor_motor = '1' then
+                    s_piso_actual <= s_piso_actual - 1;
+                    next_state <= ANALIZANDO;
+                    end if;
+                    
+                     when ANALIZANDO =>
+                       
+                       if s_piso_actual = s_piso_final then 
+                            next_state <= REPOSO;
+                       elsif s_piso_actual > s_piso_final then
+                             next_state <= BAJANDO;
+                       elsif s_piso_actual < s_piso_final then
+                             next_state <= SUBIENDO;
+                        end if;
+                    
+                 end case;      
         end if;
  end process;
+ 
+ 
+ 
+    --maquina de estado
+    maquina: process(current_state)
+     begin
+     --asignar valor 
+     motor <= "00";
+     next_state <= current_state;-- codigo de lab
+     
+     case current_state is
+     -- en reposo, asigno input a piso final
+                 when REPOSO=>
+                 motor <= "00";
+                 if s_piso_final < s_piso_actual then 
+                        next_state <= BAJANDO;--se asignara a current en state register
+                 elsif s_piso_final > s_piso_actual then 
+                        next_state <= SUBIENDO;
+                 end if;
+                 
+                 when SUBIENDO =>
+                   motor <= "01";
+                                       
+                 when  BAJANDO =>
+                   motor <= "10";
+         end case;
+                    
+   end process;
+   
+   piso_actual<= std_logic_vector(s_piso_actual);
+   piso_final<= std_logic_vector(s_piso_final);
  end architecture;
     
     
---
+++ prEstados - copia/prEstados.srcs/sources_1/new/Inst_FSM.vhd
@@ -0,0 +1,132 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity FSM is
+    port(
+    clk: in std_logic ;
+    rst: in std_logic;
+    --"REGISTROS"
+    piso_actual : out std_logic_vector(3 downto 0);
+    piso_final: out std_logic_vector(3 downto 0);
+    
+    -- SALIDA DE MAQUINA
+    motor: out std_logic_vector(1 downto 0);
+    puerta: out std_logic;
+    
+    -- ENTRADA DE USUARIO
+    planta_1, planta_2, planta_3, planta_4: in std_logic;
+    sensor_motor: in std_logic -- Detector de paso de piso
+    );
+end FSM;
+
+architecture ascensor of FSM is
+    type STATES is (REPOSO, SUBIENDO, BAJANDO, ANALIZANDO);
+    signal current_state: STATES := REPOSO;
+    signal next_state: STATES;
+ ---------------------------------------------------------------------
+    signal s_piso_actual: unsigned(3 downto 0):="0000";
+    signal s_piso_final: unsigned(3 downto 0):="0000";
+ ---------------------------------------------------------------------
+    signal next_s_piso_actual: unsigned(3 downto 0);
+    signal next_s_piso_final: unsigned(3 downto 0);
+    
+begin
+    
+
+    state_register: process (rst, clk)
+    begin
+        if rst = '1' then 
+            current_state <= REPOSO;
+            s_piso_actual <="0000";
+            s_piso_final <="0000";
+        elsif rising_edge(CLK) then
+            current_state <= next_state;
+            s_piso_actual <= next_s_piso_actual;
+            s_piso_final <= next_s_piso_final;
+        end if;
+    end process;
+
+    nextstate_ascensor: process(current_state, s_piso_actual, s_piso_final, 
+                              planta_1, planta_2, planta_3, planta_4, sensor_motor)
+    begin
+
+        next_state <= current_state;
+        next_s_piso_actual <= s_piso_actual;
+        next_s_piso_final <= s_piso_final; 
+        
+        case current_state is
+            when REPOSO =>
+                
+                if planta_1 = '1' then
+                    next_s_piso_final <= "0000";
+                elsif planta_2 = '1' then
+                    next_s_piso_final <= "0001";
+                elsif planta_3 = '1' then
+                    next_s_piso_final <= "0010";
+                elsif planta_4 = '1' then
+                    next_s_piso_final <= "0011";
+                end if;
+                
+               
+                if next_s_piso_final > s_piso_actual then
+                    next_state <= SUBIENDO;
+                elsif next_s_piso_final < s_piso_actual then
+                    next_state <= BAJANDO;
+                end if;
+
+            when SUBIENDO =>
+                
+                if sensor_motor = '1' then
+                    next_s_piso_actual <= s_piso_actual + 1; 
+                    next_state <= ANALIZANDO;
+                end if;
+
+            when BAJANDO =>
+               
+                if sensor_motor = '1' then
+                    next_s_piso_actual <= s_piso_actual - 1; 
+                    next_state <= ANALIZANDO;
+                end if;
+
+            when ANALIZANDO =>
+               
+                if s_piso_actual = s_piso_final then
+                -- if s_piso_actual = next_s_piso_final then
+                    next_state <= REPOSO; 
+                elsif s_piso_actual < s_piso_final then
+                    next_state <= SUBIENDO; 
+                else 
+                    next_state <= BAJANDO; 
+                end if;
+
+        end case;
+    end process;
+
+
+    salida: process(current_state)
+    begin
+        motor <= "00";
+        puerta <= '0';  
+        
+        case current_state is
+            when REPOSO =>
+                puerta <= '1'; 
+                motor <= "00";
+            when SUBIENDO =>
+                motor <= "01"; 
+                puerta <= '0';
+            when BAJANDO =>
+                motor <= "10"; 
+                puerta <= '0';
+            when ANALIZANDO =>
+                motor <= "00"; 
+                puerta <= '0';
+        end case;
+    end process;
+    
+    -- Asignación final a los puertos
+    piso_actual <= std_logic_vector(s_piso_actual);
+    piso_final <= std_logic_vector(s_piso_final);
+
+end architecture;
