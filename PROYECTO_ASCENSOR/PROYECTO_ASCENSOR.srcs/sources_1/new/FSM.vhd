1d93cb2449b6efecf2d0fee15489ce31e07b95bb
JunjieDai2003 <162756402+JunjieDai2003@users.noreply.github.com>
2025-12-22 18:18:03 +0100

    he fixeado el bug que imoedia salir de reposo
    proximo trabajo sera introducir un timer de 3 segundos para controlar el motor de dos maneras

--- PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/FSM.vhd
+++ PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/FSM.vhd
@@ -4,114 +4,129 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity FSM is
     port(
-    clk: in std_logic ;
-    rst: in std_logic;
-    piso_actual : out std_logic_vector(1 downto 0); -- si tengo q operar seria unsigned
-    piso_final: out std_logic_vector(1 downto 0);
-    
-    
-    --salidas de maquina
-    motor: out std_logic_vector(1 downto 0);
-    puerta: out std_logic;
-    
-    
-    -- inputs de usuario
-    planta_1, planta_2, planta_3, planta_4: in std_logic;
-    sensor_motor: in std_logic
+        clk: in std_logic ;
+        rst: in std_logic;
+
+        piso_actual : out std_logic_vector(3 downto 0);
+        piso_final  : out std_logic_vector(3 downto 0);
+
+        motor  : out std_logic_vector(1 downto 0);
+        puerta : out std_logic;
+
+        planta_1, planta_2, planta_3, planta_4: in std_logic;
+        sensor_motor: in std_logic
     );
 end FSM;
 
-architecture ascensor of fsm is
-    -- estados posibles
+architecture ascensor of FSM is
+
     type STATES is (REPOSO, SUBIENDO, BAJANDO, ANALIZANDO);
     signal current_state: STATES := REPOSO;
-    signal next_state: STATES;
-    
-    --señal de las salidas
-    signal s_piso_actual: unsigned(1 downto 0):="00";
-    signal s_piso_final: unsigned(1 downto 0):="00";
-  
-  begin
-  --registros
-   state_register: process (rst,clk)
+    signal next_state   : STATES;
+
+    signal s_piso_actual      : unsigned(3 downto 0) := (others => '0');
+    signal s_piso_final       : unsigned(3 downto 0) := (others => '0');
+
+    signal next_s_piso_actual : unsigned(3 downto 0);
+    signal next_s_piso_final  : unsigned(3 downto 0);
+
+begin
+
+    state_register: process (rst, clk)
     begin
-        if  rst = '0' then
-                current_state <= REPOSO;
-                s_piso_actual <="00";
-                s_piso_final <="00";
-                
-         elsif rising_edge(CLK) then
-                current_state <= next_state;
-                
-                case current_state is
-                
-                  when REPOSO =>
-                    if planta_1 = '1' 
-                            then s_piso_final <= "00";
-                    elsif planta_2 = '1' 
-                            then s_piso_final <= "01";
-                    elsif planta_3 = '1' 
-                            then s_piso_final <= "10";
-                    elsif planta_4 = '1' 
-                            then s_piso_final <= "11";
-                    end if;
-                
-                    when SUBIENDO =>
-                    if sensor_motor = '1' then
-                    s_piso_actual <= s_piso_actual + 1;
-                    next_state <= ANALIZANDO;
-                    end if;
-                    
-                    when BAJANDO => 
-                    if sensor_motor = '1' then
-                    s_piso_actual <= s_piso_actual - 1;
-                    next_state <= ANALIZANDO;
-                    end if;
-                    
-                     when ANALIZANDO =>
-                       
-                       if s_piso_actual = s_piso_final then 
-                            next_state <= REPOSO;
-                       elsif s_piso_actual > s_piso_final then
-                             next_state <= BAJANDO;
-                       elsif s_piso_actual < s_piso_final then
-                             next_state <= SUBIENDO;
-                        end if;
-                    
-                 end case;      
+        if rst = '0' then 
+            current_state   <= REPOSO;
+            s_piso_actual   <= (others => '0');
+            s_piso_final    <= (others => '0');
+        elsif rising_edge(clk) then
+            current_state   <= next_state;
+            s_piso_actual   <= next_s_piso_actual;
+            s_piso_final    <= next_s_piso_final;
         end if;
- end process;
- 
- 
- 
-    --maquina de estado
-    maquina: process(current_state)
-     begin
-     --asignar valor 
-     motor <= "00";
-     next_state <= current_state;-- codigo de lab
-     
-     case current_state is
-     -- en reposo, asigno input a piso final
-                 when REPOSO=>
-                 motor <= "00";
-                 if s_piso_final < s_piso_actual then 
-                        next_state <= BAJANDO;--se asignara a current en state register
-                 elsif s_piso_final > s_piso_actual then 
-                        next_state <= SUBIENDO;
-                 end if;
-                 
-                 when SUBIENDO =>
-                   motor <= "01";
-                                       
-                 when  BAJANDO =>
-                   motor <= "10";
-         end case;
-                    
-   end process;
-   
-   piso_actual<= std_logic_vector(s_piso_actual);
-   piso_final<= std_logic_vector(s_piso_final);
- end architecture;
-    
-    
+    end process;
+
+    nextstate_ascensor: process(current_state, s_piso_actual, s_piso_final,
+                                planta_1, planta_2, planta_3, planta_4, sensor_motor)
+        --variable para cambiar su valor dentro de process y hacer comparacion en reposo                        
+        variable objetivo : unsigned(3 downto 0);
+    begin
+        next_state           <= current_state;
+        next_s_piso_actual   <= s_piso_actual;
+        next_s_piso_final    <= s_piso_final;
+
+        case current_state is
+            when REPOSO =>
+                
+                objetivo := s_piso_final; 
+
+                if planta_1 = '1' then
+                   objetivo := "0000"; 
+                elsif planta_2 = '1' then
+                   objetivo := "0001"; 
+                elsif planta_3 = '1' then
+                  objetivo := "0010";
+                elsif planta_4 = '1' then
+                   objetivo := "0011"; 
+                end if;
+                
+               --cargo el valor para "llevarlo fuera"
+                next_s_piso_final <= objetivo; 
+
+                -- comparo
+                if objetivo  > s_piso_actual then
+                    next_state <= SUBIENDO; 
+                elsif objetivo  < s_piso_actual then
+                    next_state <= BAJANDO; 
+                elsif objetivo  = s_piso_actual then
+                     next_state <= REPOSO; 
+                end if;
+
+            when SUBIENDO =>
+                if sensor_motor = '1' then
+                    next_s_piso_actual <= s_piso_actual + 1;
+                    next_state <= ANALIZANDO;
+                end if;
+
+            when BAJANDO =>
+                if sensor_motor = '1' then
+                    next_s_piso_actual <= s_piso_actual - 1;
+                    next_state <= ANALIZANDO;
+                end if;
+
+            when ANALIZANDO =>
+                
+                if s_piso_actual = s_piso_final then 
+                    next_state <= REPOSO; 
+                elsif s_piso_actual < s_piso_final then 
+                    next_state <= SUBIENDO;
+                else
+                    next_state <= BAJANDO; 
+                end if;
+        end case;
+    end process;
+
+    salida: process(current_state)
+    begin
+        motor  <= "00";
+        puerta <= '0';
+        case current_state is
+            when REPOSO =>
+                puerta <= '1';
+                motor  <= "00";
+            when SUBIENDO =>
+                motor  <= "01";
+                puerta <= '0';
+            when BAJANDO =>
+                motor  <= "10";
+                puerta <= '0';
+            when ANALIZANDO =>
+                motor  <= "00";
+                puerta <= '0';
+                
+    end case;
+    end process;
+
+    piso_actual <= std_logic_vector(s_piso_actual);
+    piso_final  <= std_logic_vector(s_piso_final);
+
+end architecture;
--- PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/TOP.vhd
+++ PROYECTO_ASCENSOR/PROYECTO_ASCENSOR.srcs/sources_1/new/TOP.vhd
@@ -1,44 +1,143 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05.12.2025 17:25:10
--- Design Name: 
--- Module Name: TOP - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity top is
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+PORT(
+ clk : IN std_logic;
+ rst : IN std_logic;
+p1,p2,p3,p4: in std_logic;
+sensor: in std_logic;
+motor: out std_logic_vector(1 downto 0);
+puerta: out std_logic;
+--digctrl : OUT std_logic_vector(3 DOWNTO 0);--numero
+segment : OUT std_logic_vector(6 DOWNTO 0) -- display
+);
+end top;
 
-entity TOP is
-    Port ( SW : in STD_LOGIC_VECTOR (15 downto 0);
-           LED : out STD_LOGIC_VECTOR (49 downto 0));
-end TOP;
+architecture structural of top is
+signal syn_edg1, syn_edg2,syn_edg3 ,syn_edg4,syn_edg_motor: STD_LOGIC;
+SIGNAL edg_fsm1,edg_fsm2,edg_fsm3,edg_fsm4,edg_fsm_motor: std_logic;
+signal piso_actual_s : std_logic_vector(3 downto 0);
+signal piso_final_s  : std_logic_vector(3 downto 0);
+--signal code_in : std_logic_vector(3 downto 0);
 
-architecture Behavioral of TOP is
+COMPONENT decoder
+PORT (
+code : IN std_logic_vector(3 DOWNTO 0);
+led : OUT std_logic_vector(6 DOWNTO 0)
+);
+end component;
 
-begin
+COMPONENT FSM is 
+port(
+clk: in std_logic ;
+    rst: in std_logic;
+    --"REGISTROS"
+    piso_actual : out std_logic_vector(3 downto 0);
+    piso_final: out std_logic_vector(3 downto 0);
+    
+    -- SALIDA DE MAQUINA
+    motor: out std_logic_vector(1 downto 0);
+    puerta: out std_logic;
+    
+    -- ENTRADA DE USUARIO
+    planta_1, planta_2, planta_3, planta_4: in std_logic;
+    sensor_motor: in std_logic -- Detector de paso de piso
+);
+END COMPONENT;
 
 
-end Behavioral;
+COMPONENT SYNCHRNZR is
+PORT(
+CLK : IN std_logic;
+ASYNC_IN : IN std_logic;
+SYNC_OUT : OUT std_logic
+);
+END COMPONENT;
+
+COMPONENT EDGEDTCTR is
+PORT ( 
+CLK : IN std_logic;
+SYNC_IN : IN std_logic;
+EDGE : OUT std_logic
+);
+
+END COMPONENT;
+
+begin 
+
+Inst_FSM: FSM PORT MAP (
+rst => rst,
+CLK => clk,
+planta_1 => edg_fsm1,
+planta_2 => edg_fsm2,
+planta_3 => edg_fsm3,
+planta_4 => edg_fsm4,
+sensor_motor => edg_fsm_motor,
+--salidas
+puerta => puerta,
+piso_actual => piso_actual_s,
+piso_final =>piso_final_s,
+motor => motor
+);
+
+Inst_decoder : decoder PORT MAP ( 
+code => piso_actual_s,
+led => segment
+);
+Inst_SYNCHRNZR_MOTOR : SYNCHRNZR PORT MAP ( 
+CLK => clk,
+ASYNC_IN => sensor,
+SYNC_OUT => syn_edg_motor
+);
+Inst_SYNCHRNZR1 : SYNCHRNZR PORT MAP ( 
+CLK => clk,
+ASYNC_IN => P1,
+SYNC_OUT => syn_edg1
+);
+Inst_SYNCHRNZR2 : SYNCHRNZR PORT MAP ( 
+CLK => clk,
+ASYNC_IN => P2,
+SYNC_OUT => syn_edg2
+);
+Inst_SYNCHRNZR3 : SYNCHRNZR PORT MAP ( 
+CLK => clk,
+ASYNC_IN => P3,
+SYNC_OUT => syn_edg3
+);
+Inst_SYNCHRNZR4 : SYNCHRNZR PORT MAP ( 
+CLK => clk,
+ASYNC_IN => P4,
+SYNC_OUT => syn_edg4
+);
+
+Inst_EDGEDTCTR_MOTOR : EDGEDTCTR PORT MAP ( 
+CLK => clk,
+SYNC_IN => syn_edg_motor,
+EDGE => edg_fsm_motor
+);
+
+Inst_EDGEDTCTR1 : EDGEDTCTR PORT MAP ( 
+CLK => clk,
+SYNC_IN => syn_edg1,
+EDGE => edg_fsm1
+);
+Inst_EDGEDTCTR2 : EDGEDTCTR PORT MAP ( 
+CLK => clk,
+SYNC_IN => syn_edg2,
+EDGE => edg_fsm2
+);
+Inst_EDGEDTCTR3 : EDGEDTCTR PORT MAP ( 
+CLK => clk,
+SYNC_IN => syn_edg3,
+EDGE => edg_fsm3
+);
+Inst_EDGEDTCTR4 : EDGEDTCTR PORT MAP ( 
+CLK => clk,
+SYNC_IN => syn_edg4,
+EDGE => edg_fsm4
+);
+
+
+end structural;
